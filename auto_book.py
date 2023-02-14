e47076ef8c24090d018bffd02fa901c5b8476ccc
tdegold <timdegold@gmx.net>
2023-02-14 13:00:05 +0100

    added read-csv part

--- auto_book.py
+++ auto_book.py
@@ -1,5 +1,8 @@
 import sqlite3
 from pprint import pprint
+import pandas as pd
+
+##################### DB STUFF ################################################
 
 def clean_dump(file: str, write_to_file="") -> str:
     """Returns the content of a SQL-dump file but removes parts sqlite cannot correctly interpret
@@ -48,13 +51,59 @@ def seed_db(script: str, connection_string=":memory:") -> sqlite3.Connection:
     cur.executescript(script)
     return con
 
+############### DATA STUFF #################################################
+
+def get_distinct_titles(con: sqlite3.Connection) -> list:
+    res = con.cursor().execute("SELECT DISTINCT(titel) FROM table_mitglieder")
+    # filter empty titel-fields
+    return [item for t in res for item in t if item not in ("", " ", None)]
+
+def parse_fullinfo(text: str) -> pd.Series:
+    """Parse the single column text into ["sender", "text", "reference", "iban", "bic"].
+    
+    :param text: contains all the neccessary information as one string
+    :return: returns the information as separate columns
+    """
+    if text.find("Auftraggeber: ") == 0:
+        text = text[14:]    # drop the leading `Auftraggeber: `
+
+    ref = info = bic = iban = None
+    if " BIC Auftraggeber: " in text:
+        text, bic = text.split(" BIC Auftraggeber: ")
+    if " IBAN Auftraggeber: " in text:
+        text, iban = text.split(" IBAN Auftraggeber: ")
+    if " Zahlungsreferenz: " in text:
+        text, ref = text.split(" Zahlungsreferenz: ")
+    if " Verwendungszweck: " in text:
+        text, info = text.split(" Verwendungszweck: ")
+
+    return pd.Series([text, info, ref, iban, bic], ["sender", "text", "reference", "iban", "bic"])
+
+def read_and_parse_csv(file: str) -> pd.core.frame.DataFrame:
+    df_eh = pd.read_csv(
+        file, delimiter=";", decimal=",", header=None,
+        usecols=[0, 1, 3, 4],
+        names=["date", "fullinfo", "amount", "currency"]
+    )
+
+    # drop `outgoing` rows
+    df_eh = df_eh.loc[df_eh.amount > 0]
+
+    # parse the information into separate columns
+    parsed_info = df_eh.fullinfo.apply(lambda x: parse_fullinfo(x))
+    df_eh = pd.concat([df_eh, parsed_info], axis=1)
+    df_eh = df_eh.drop(columns=["fullinfo"])
+
+    # set column order
+    df_eh = df_eh[["date", "sender", "amount", "currency", "reference", "text", "iban", "bic"]]
+
+    return df_eh
 
 if __name__ == "__main__":
     file = input("Welche sql-dump Datei soll bereinigt werden: ")
-    name = input("Bitte den Nachnamen eines Mitglieds angeben: ")
     script = clean_dump(file)
-    cur = seed_db(script).cursor()
-    cur.execute(f"SELECT * FROM table_mitglieder WHERE nachname='{name}'")
-    res = cur.fetchall()
-    pprint(res)
+    con = seed_db(script)
+    titel = get_distinct_titles(con)
+    df_eh = read_and_parse_csv("daten.csv")
+    
 
