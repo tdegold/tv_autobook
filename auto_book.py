7126850283e1c589fa63b16f604d3517aafaa448
tdegold <timdegold@gmx.net>
2023-04-20 20:18:09 +0200

    some code cleanup

--- auto_book.py
+++ auto_book.py
@@ -20,36 +20,33 @@ for p in df_eh.iloc:
     status, id = pred.match_id(p)
     
     if status == 1:
+        amount, donation = split_fee(int(p.amount), fee)
 
-        with open("output.sql", "a") as output:
-
-            amount, donation = split_fee(int(p.amount), fee)
-            
+        with open("output.sql", "a", encoding="utf-8") as output:
             output.write(insert_fee(id, p.date, amount))
             if donation > 0:
                 output.write(insert_don(id, p.date, donation))
 
-        with open("log.txt", "a") as log:
+        with open("log.txt", "a", encoding="utf-(") as log:
             log.writelines(str(p))
             log.write(f"\nDatensatz wurde automatisch gebucht mit MID {id}. Betrag={amount}. Spende={donation}.\n\n")
 
     elif status == 2:
-        with open("log.txt", "a") as log:
+        with open("log.txt", "a", encoding="utf-(") as log:
             log.writelines(str(p))
             log.write(f"\nDatensatz wurde übersprungen, da bereits eine Buchung in diesem Jahr vorhanden ist.\n\n")
 
     else:
         print(p)
 
-
         condition = True
         while condition:
             u_in = input("Es konnte keine ID ermittelt werden. Bitte angeben oder \"skip\": ")
             if u_in.__eq__("skip"):
-                with open("log.txt", "a") as log:
+                with open("log.txt", "a", encoding="utf-(") as log:
                         log.writelines(str(p))
                         log.write(f"\nDatensatz wurde vom User übersprungen.\n\n")
-                print("Datensatz wurde übersprungen. Keine Buchung.")
+                print("Datensatz wurde übersprungen. Keine Buchung.\n")
                 condition = False
 
             try:
@@ -62,7 +59,7 @@ for p in df_eh.iloc:
 
                 ##################################
                 if code == 1:
-                    with open("output.sql", "a") as output:
+                    with open("output.sql", "a", encoding="utf-(") as output:
 
                         amount, donation = split_fee(int(p.amount), fee)
             
@@ -70,34 +67,31 @@ for p in df_eh.iloc:
                         if donation > 0:
                             output.write(insert_don(id, p.date, donation))
                 
-                    with open("log.txt", "a") as log:
+                    with open("log.txt", "a", encoding="utf-(") as log:
                         log.writelines(str(p))
                         log.write(f"\nDatensatz wurde vom User gebucht mit MID {id}. Betrag={amount}. Spende={donation}.\n\n")
                 
-                    print(f"\nDatensatz wurde vom User gebucht mit MID {id}. Betrag={amount}. Spende={donation}.")
+                    print(f"Datensatz wurde vom User gebucht mit MID {id}. Betrag={amount}. Spende={donation}.\n")
                     condition = False
                 ######################################
                 elif code == 2:
-                    with open("log.txt", "a") as log:
+                    with open("log.txt", "a", encoding="utf-(") as log:
                         log.writelines(str(p))
                         log.write(f"\nDatensatz wurde übersprungen, da bereits eine Buchung in diesem Jahr vorhanden ist.\n\n")
 
-                    print(f"\nDatensatz wurde übersprungen, da bereits eine Buchung in diesem Jahr vorhanden ist.\n\n") 
+                    print(f"Datensatz wurde übersprungen, da bereits eine Buchung in diesem Jahr vorhanden ist.\n\n") 
                     condition = False
                 ##########################################
                 elif code == -2:
-                    print(f"Der Name stimmt überein, die Person ist aber in mind. einer ungewollten Gruppe {p.groups.intersection(pred.exclude_groups)}. Datensatz wird übersprungen.")
+                    print(f"Der Name stimmt überein, die Person ist aber in mind. einer ungewollten Gruppe {p.groups.intersection(pred.exclude_groups)}. Datensatz wird übersprungen.\n")
 
-                    with open("log.txt", "a") as log:
+                    with open("log.txt", "a", encoding="utf-(") as log:
                         log.writelines(str(p))
                         log.write(f"\nDatensatz wurde übersprungen, da die Person in mind. einer ungewollten Gruppe ist. ({p.groups.intersection(pred.exclude_groups)})")
                     condition = False
                 ###################################################
                 else:
-                    print(f"Der Name stimmt nicht überein. Gefunden wurde eine Person mit Nachnamen\"{cand_su}\" und MID {cand_id}.")
-  
+                    print(f"Der Name stimmt nicht überein. Gefunden wurde eine Person mit Nachnamen \"{cand_su}\" und MID {cand_id}.\n")
             except ValueError:
                 pass
-            
-
             
